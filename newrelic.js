89c06cd7dd4266b865c042cd26022abb50b6eb43
Andrew Hobden <andrew@hoverbear.org>
2013-10-28 13:46:18 -0700

    Don't include a newrelic key by default.

--- newrelic.js
+++ newrelic.js
@@ -15,7 +15,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key : config.newrelic_Key,
+  /*license_key : "",*/
   logging : {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
